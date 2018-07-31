f62415e4422a35bd70657a890104ad44e25e2a02
xiaochunping <xcp.email@gmail.com>
2018-07-31 22:43:03 +0800

    增加front的fdfs支持

--- mall-front-web/src/main/java/com/dongnaoedu/mall/front/controller/GoodsController.java
+++ mall-front-web/src/main/java/com/dongnaoedu/mall/front/controller/GoodsController.java
@@ -15,6 +15,7 @@ import com.dongnaoedu.mall.manager.dto.front.AllGoodsResult;
 import com.dongnaoedu.mall.manager.dto.front.ProductDet;
 import com.dongnaoedu.mall.manager.dto.front.SearchResult;
 import com.dongnaoedu.mall.manager.pojo.TbPanel;
+import com.dongnaoedu.mall.manager.pojo.TbPanelContent;
 import com.dongnaoedu.mall.search.service.SearchService;
 
 import io.swagger.annotations.Api;
@@ -27,62 +28,81 @@ import io.swagger.annotations.ApiOperation;
 @Api(description = "商品页面展示")
 public class GoodsController {
 
-	@Autowired
-	private ContentService contentService;
-	@Autowired
-	private SearchService searchService;
+    @Autowired
+    private ContentService contentService;
+    @Autowired
+    private SearchService searchService;
 
-	@RequestMapping(value = "/goods/home", method = RequestMethod.GET)
-	@ApiOperation(value = "首页内容展示")
-	public Result<List<TbPanel>> getProductHome() {
+    @RequestMapping(value = "/goods/navList",method = RequestMethod.GET)
+    @ApiOperation(value = "获取导航栏")
+    public Result<List<TbPanelContent>> getNavList(){
 
-		List<TbPanel> list = contentService.getHome();
-		return new ResultUtil<List<TbPanel>>().setData(list);
-	}
+        List<TbPanelContent> list=contentService.getNavList();
+        return new ResultUtil<List<TbPanelContent>>().setData(list);
+    }
 
-	@RequestMapping(value = "/goods/productDet", method = RequestMethod.GET)
-	@ApiOperation(value = "商品详情")
-	public Result<ProductDet> getProductDet(Long productId) {
+    @RequestMapping(value = "/goods/home",method = RequestMethod.GET)
+    @ApiOperation(value = "首页内容展示")
+    public Result<List<TbPanel>> getProductHome(){
 
-		ProductDet productDet = contentService.getProductDet(productId);
-		return new ResultUtil<ProductDet>().setData(productDet);
-	}
+        List<TbPanel> list=contentService.getHome();
+        return new ResultUtil<List<TbPanel>>().setData(list);
+    }
 
-	@RequestMapping(value = "/goods/allGoods", method = RequestMethod.GET)
-	@ApiOperation(value = "所有商品")
-	public Result<AllGoodsResult> getAllProduct(@RequestParam(defaultValue = "1") int page,
-			@RequestParam(defaultValue = "20") int size, @RequestParam(defaultValue = "") String sort,
-			@RequestParam(defaultValue = "") Long cid, @RequestParam(defaultValue = "-1") int priceGt,
-			@RequestParam(defaultValue = "-1") int priceLte) {
+    @RequestMapping(value = "/goods/productDet",method = RequestMethod.GET)
+    @ApiOperation(value = "商品详情")
+    public Result<ProductDet> getProductDet(Long productId){
 
-		AllGoodsResult allGoodsResult = contentService.getAllProduct(page, size, sort, cid, priceGt, priceLte);
-		return new ResultUtil<AllGoodsResult>().setData(allGoodsResult);
-	}
+        ProductDet productDet=contentService.getProductDet(productId);
+        return new ResultUtil<ProductDet>().setData(productDet);
+    }
 
-	@RequestMapping(value = "/goods/search", method = RequestMethod.GET)
-	@ApiOperation(value = "搜索商品ES")
-	public Result<SearchResult> searchProduct(@RequestParam(defaultValue = "") String key,
-			@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "20") int size,
-			@RequestParam(defaultValue = "") String sort, @RequestParam(defaultValue = "-1") int priceGt,
-			@RequestParam(defaultValue = "-1") int priceLte) {
+    @RequestMapping(value = "/goods/allGoods",method = RequestMethod.GET)
+    @ApiOperation(value = "所有商品")
+    public Result<AllGoodsResult> getAllProduct(@RequestParam(defaultValue = "1") int page,
+                                                @RequestParam(defaultValue = "20") int size,
+                                                @RequestParam(defaultValue = "") String sort,
+                                                @RequestParam(defaultValue = "") Long cid,
+                                                @RequestParam(defaultValue = "-1") int priceGt,
+                                                @RequestParam(defaultValue = "-1") int priceLte){
 
-		SearchResult searchResult = searchService.search(key, page, size, sort, priceGt, priceLte);
-		return new ResultUtil<SearchResult>().setData(searchResult);
-	}
+        AllGoodsResult allGoodsResult=contentService.getAllProduct(page,size,sort,cid,priceGt,priceLte);
+        return new ResultUtil<AllGoodsResult>().setData(allGoodsResult);
+    }
 
-	@RequestMapping(value = "/goods/recommend", method = RequestMethod.GET)
-	@ApiOperation(value = "商品推荐板块")
-	public Result<List<TbPanel>> getRecommendGoods() {
+    @RequestMapping(value = "/goods/search",method = RequestMethod.GET)
+    @ApiOperation(value = "搜索商品ES")
+    public Result<SearchResult> searchProduct(@RequestParam(defaultValue = "") String key,
+                                              @RequestParam(defaultValue = "1") int page,
+                                              @RequestParam(defaultValue = "20") int size,
+                                              @RequestParam(defaultValue = "") String sort,
+                                              @RequestParam(defaultValue = "-1") int priceGt,
+                                              @RequestParam(defaultValue = "-1") int priceLte){
 
-		List<TbPanel> list = contentService.getRecommendGoods();
-		return new ResultUtil<List<TbPanel>>().setData(list);
-	}
+        SearchResult searchResult=searchService.search(key,page,size,sort,priceGt,priceLte);
+        return new ResultUtil<SearchResult>().setData(searchResult);
+    }
 
-	@RequestMapping(value = "/goods/thank", method = RequestMethod.GET)
-	@ApiOperation(value = "我要捐赠板块")
-	public Result<List<TbPanel>> getThankGoods() {
+    @RequestMapping(value = "/goods/recommend",method = RequestMethod.GET)
+    @ApiOperation(value = "商品推荐板块")
+    public Result<List<TbPanel>> getRecommendGoods(){
 
-		List<TbPanel> list = contentService.getThankGoods();
-		return new ResultUtil<List<TbPanel>>().setData(list);
-	}
+        List<TbPanel> list=contentService.getRecommendGoods();
+        return new ResultUtil<List<TbPanel>>().setData(list);
+    }
+
+    @RequestMapping(value = "/goods/thank",method = RequestMethod.GET)
+    @ApiOperation(value = "我要捐赠板块")
+    public Result<List<TbPanel>> getThankGoods(){
+
+        List<TbPanel> list=contentService.getThankGoods();
+        return new ResultUtil<List<TbPanel>>().setData(list);
+    }
+
+    @RequestMapping(value = "/goods/quickSearch",produces= "text/plain;charset=UTF-8",method = RequestMethod.GET)
+    @ApiOperation(value = "快速搜索")
+    public String getQuickSearch(@RequestParam(defaultValue = "") String key){
+
+        return searchService.quickSearch(key);
+    }
 }
