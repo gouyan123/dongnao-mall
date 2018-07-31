d2a8173d139eacf14ef309a020e327a0fbe03c9e
xiaochunping <xcp.email@gmail.com>
2018-07-31 22:47:05 +0800

    修改es地址，增加restful接口支持

--- mall-search/mall-search-interface/src/main/java/com/dongnaoedu/mall/search/service/SearchService.java
+++ mall-search/mall-search-interface/src/main/java/com/dongnaoedu/mall/search/service/SearchService.java
@@ -9,7 +9,6 @@ public interface SearchService {
 
 	/**
 	 * ES商品搜索
-	 * 
 	 * @param keyword
 	 * @param page
 	 * @param size
@@ -18,5 +17,12 @@ public interface SearchService {
 	 * @param priceLte
 	 * @return
 	 */
-	SearchResult search(String keyword, int page, int size, String sort, int priceGt, int priceLte);
+	SearchResult search(String keyword, int page, int size,String sort,int priceGt,int priceLte);
+
+	/**
+	 * restful搜索接口
+	 * @param key
+	 * @return
+	 */
+	String quickSearch(String key);
 }
