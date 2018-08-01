e42f27c58a74932e4d62da0f869c08bb6c555d1f
xiaochunping <xcp.email@gmail.com>
2018-08-01 22:57:33 +0800

    增加fdfs支持

--- mall-sso/mall-sso-service/src/main/java/com/dongnaoedu/mall/sso/config/DubboConfig.java
+++ mall-sso/mall-sso-service/src/main/java/com/dongnaoedu/mall/sso/config/DubboConfig.java
@@ -12,6 +12,6 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:dubbo.properties")
 @ImportResource({ "classpath:dubbo/*.xml" })
-public class PropertiesConfig {
+public class DubboConfig {
 
 }
